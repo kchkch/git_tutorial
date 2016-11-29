f4c79a7db32ebc3da9b176d1c513a672b8f68eb6
Kim CH <jyug13@naver.com>
2016-11-29 11:16:53 +0900

    Hello his git

--- Hello.java
+++ Hello.java
@@ -2,7 +2,8 @@
 public class Hello{
 	public static void main(String[] args){
 		System.out.println("Hello Git");
-		System.out.println("Hello my Git"):
+		System.out.println("Hello my Git");
+		System.out.println("Hello his Git");
 	}
 }
 
